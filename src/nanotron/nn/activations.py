c065d76df0f8e95636905f1cb71af4682828085a
Allen <allenhuangdd@gmail.com>
2024-11-25 06:41:31 +1300

    testing fixed alpha_p

--- src/nanotron/nn/activations.py
+++ src/nanotron/nn/activations.py
@@ -43,18 +43,19 @@ class XIELU(nn.Module):
 
 
 class XIELUv2(nn.Module):
-    def __init__(self, alpha_p_init=2.0, alpha_n_init=0.8, beta=0.5):
+    def __init__(self, alpha_p_init=2.0, alpha_n_init=0.8, beta=0.5, eps=-1e-6):
         super(XIELUv2, self).__init__()
         self.beta = beta
-        self.alpha_p = NanotronParameter(torch.log(torch.tensor(alpha_p_init)) - 1)
+        self.alpha_p = alpha_p_init # NanotronParameter(torch.log(torch.tensor(alpha_p_init)) - 1)
         self.alpha_n = NanotronParameter(torch.log(torch.tensor(alpha_n_init - self.beta)) - 1)
+        self.eps = torch.tensor(eps)
 
     def forward(self, x):
-        alpha_p = F.softplus(self.alpha_p)
+        alpha_p = self.alpha_p # F.softplus(self.alpha_p)
         alpha_n = self.beta + F.softplus(self.alpha_n)
-        return torch.where(x > -1e-6,
+        return torch.where(x > 0,
                            alpha_p * x * x + self.beta * x,
-                           alpha_n * torch.expm1(x) - alpha_n * x + self.beta * x)
+                           alpha_n * torch.expm1(torch.min(x, self.eps)) - alpha_n * x + self.beta * x)
 
 
 class PytorchGELUTanh(nn.Module):
